bc0c0739725f2f8240f20bf0fd40a4b1ccc3d100
Giorgio Gallo <giorgio.gallo@bitnic.it>
2018-01-30 19:00:41 +0100

    Removed MaxOf and MinOf. Made Max and Min variadic instead

--- functions.go
+++ functions.go
@@ -6,7 +6,7 @@ import (
 
 // Min returns the min index and value in the given values.
 // Returns (-1, NaN) if called with an empty slice.
-func Min(values []float64) (int,float64) {
+func Min(values ...float64) (int,float64) {
 	if len(values) == 0 { return -1, math.NaN() }
 	mi := -1
 	mv := math.Inf(1)
@@ -18,7 +18,7 @@ func Min(values []float64) (int,float64) {
 
 // Max returns the max index and value in the given values.
 // Returns (-1, NaN) if called with an empty slice.
-func Max(values []float64) (int,float64) {
+func Max(values ...float64) (int,float64) {
 	if len(values) == 0 { return -1, math.NaN() }
 	mi := -1
 	mv := math.Inf(-1)
@@ -26,14 +26,4 @@ func Max(values []float64) (int,float64) {
 		if v > mv { mv = v ; mi = i }
 	}
 	return mi,mv
-}
-
-// MinOf is a convenience, variadic version of Min().
-func MinOf(values ...float64) (int,float64) {
-	return Min(values)
-}
-
-// MaxOf is a convenience, variadic version of Max().
-func MaxOf(values ...float64) (int,float64) {
-	return Max(values)
 }
--- functions_test.go
+++ functions_test.go
@@ -5,49 +5,25 @@ import (
 )
 
 func TestMin1(t *testing.T) {
-	i,v := Min([]float64{0,1,2})
-	if i != 0 { t.Errorf("wrong index") }
-	if v != 0 { t.Errorf("wrong value") }
-}
-
-func TestMinOf1(t *testing.T) {
-	i,v := MinOf(0,1,2)
+	i,v := Min(0,1,2)
 	if i != 0 { t.Errorf("wrong index") }
 	if v != 0 { t.Errorf("wrong value") }
 }
 
 func TestMin2(t *testing.T) {
-	i,v := Min([]float64{2,1,0})
-	if i != 2 { t.Errorf("wrong index") }
-	if v != 0 { t.Errorf("wrong value") }
-}
-
-func TestMinOf2(t *testing.T) {
-	i,v := MinOf(2,1,0)
+	i,v := Min(2,1,0)
 	if i != 2 { t.Errorf("wrong index") }
 	if v != 0 { t.Errorf("wrong value") }
 }
 
 func TestMax1(t *testing.T) {
-	i,v := Max([]float64{0,1,2})
-	if i != 2 { t.Errorf("wrong index") }
-	if v != 2 { t.Errorf("wrong value") }
-}
-
-func TestMaxOf1(t *testing.T) {
-	i,v := MaxOf(0,1,2)
+	i,v := Max(0,1,2)
 	if i != 2 { t.Errorf("wrong index") }
 	if v != 2 { t.Errorf("wrong value") }
 }
 
 func TestMax2(t *testing.T) {
-	i,v := Max([]float64{2,1,0})
-	if i != 0 { t.Errorf("wrong index") }
-	if v != 2 { t.Errorf("wrong value") }
-}
-
-func TestMaxOf2(t *testing.T) {
-	i,v := MaxOf(2,1,0)
+	i,v := Max(2,1,0)
 	if i != 0 { t.Errorf("wrong index") }
 	if v != 2 { t.Errorf("wrong value") }
 }
