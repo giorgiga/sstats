13fa2a153768945707c864fc496c66208e1c605b
Giorgio Gallo <giorgio.gallo@bitnic.it>
2018-01-30 19:12:56 +0100

    Added more tests.

--- functions_test.go
+++ functions_test.go
@@ -27,3 +27,13 @@ func TestMax2(t *testing.T) {
 	if i != 0 { t.Errorf("wrong index") }
 	if v != 2 { t.Errorf("wrong value") }
 }
+
+func TestMinEq(t *testing.T) {
+	i,_ := Min(0,0,0)
+	if i != 0 { t.Errorf("wrong index") }
+}
+
+func TestMaxEq(t *testing.T) {
+	i,_ := Max(0,0,0)
+	if i != 0 { t.Errorf("wrong index") }
+}
